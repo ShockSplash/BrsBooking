2bdba3067d90e921112648bb7419d62c6bb0e14c
Artem <75182193+ShockSplash@users.noreply.github.com>
2021-05-02 11:30:53 +0400

    Creat script file
    Добавил папку с sql скриптом создания бд

---
+++ SQL script/Script.sql
@@ -0,0 +1,25 @@
+    create table Hotel(
+  id serial primary key,
+  name varchar(40) not null,
+  city varchar(40) not null,
+  description varchar(200) not null,
+  isFreeRooms bool not null);
+
+create table Room(
+  id serial primary key,
+  h_id integer references hotel(id) on delete cascade,
+  seats integer not null,
+  price float not null);
+
+create table users(
+  id serial primary key,
+  name varchar(60) not null,
+  login varchar(60) not null,
+  password varchar(60) not null);
+  
+  create table booking(
+  idOfRoom integer references room(id) on delete cascade,
+  beginDate date not null,
+  endDate date not null,
+  userId integer references users(id) on delete cascade
+  );
